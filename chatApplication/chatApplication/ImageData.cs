ae772f0eb857eb67eb60eea048176bd7a38da616
Imtiaz Faisal <imtiaz.faisal05@gmail.com>
2021-10-02 21:38:20 +0600

    first commit

---
+++ chatApplication/chatApplication/ImageData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chatApplication
+{
+    public class ImageData
+    {
+        public string Filename { get; set; }
+        public string Image { get; set; }
+    }
+}
